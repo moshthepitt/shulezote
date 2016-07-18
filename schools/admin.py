7441c9e46c94ef17af70282c826f1a437320fed0
moshthepitt <kelvin@jayanoris.com>
2016-07-18 15:50:29 +0300

    add description to School admin

--- schools/admin.py
+++ schools/admin.py
@@ -1,9 +1,20 @@
 from django.contrib.gis import admin
+from django.forms import ModelForm
+
+from suit_redactor.widgets import RedactorWidget
 
 from schools.models import School
 
 
+class SchoolForm(ModelForm):
+    class Meta:
+        widgets = {
+            'description': RedactorWidget(editor_options={'lang': 'en'}),
+        }
+
+
 class SchoolAdmin(admin.GeoModelAdmin):
+    # form = SchoolForm
     search_fields = ['name']
     list_filter = ['level']
 

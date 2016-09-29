3192dcb2ca90ccafb09b588196ca29dae4066871
moshthepitt <kelvin@jayanoris.com>
2016-09-29 18:53:12 +0300

    add amp page

--- schools/urls.py
+++ schools/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.views.decorators.cache import cache_page
 
 from schools.views import SchoolView
 
-urlpatterns = patterns('',
+urlpatterns = [
+    url(r'^(?P<slug>[\w-]+)/amp$', cache_page(60 * 60 * 24)
+        (SchoolView.as_view(template_name="schools/school.amp.html")), name='school_amp'),
     url(r'^(?P<slug>[\w-]+)/$', cache_page(60 * 60 * 24)(SchoolView.as_view()), name='school'),
-)
+]

157add41517055b37621778df92dfacd0cd421e2
Kelvin Jayanoris <kjayanoris@gmail.com>
2019-08-04 12:24:44 +0300

    Add ads.txt to url

--- template/urls.py
+++ template/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 
 from django.conf import settings
 
-from core.views import HomePageView
+from core.views import HomePageView, ads_txt_view
 from core.sitemaps import sitemaps
 
 # sitemap_urls = patterns('django.contrib.sitemaps.views',
@@ -28,6 +28,7 @@ urlpatterns = patterns('',
     # url(r'^accounts/', include('allauth.urls')),
     url(r'^page/', include('django.contrib.flatpages.urls')),
     url(r'^search/', include('haystack.urls')),
+    url(r'^ads\.txt$', ads_txt_view, name='ads.txt'),
 )
 
 urlpatterns = urlpatterns + sitemap_urls
